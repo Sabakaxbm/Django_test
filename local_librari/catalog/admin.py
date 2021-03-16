ef4e0c227b1bfc386461ff3149fcdeed695a1dca
Sabakaxbm <soba4kaxbm@mail.ru>
2021-03-16 20:52:15 +0800

    First site

---
+++ local_librari/catalog/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class AuthorBooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [AuthorBooksInline]
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+    inlines = [BooksInstanceInline]
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'borrower', id)
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Доступность', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+    pass
+
+
---
+++ local_librari/catalog/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+from django.contrib.auth.models import User
+from datetime import date
+from django import forms
+
+# Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Введите жанр книги")
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Описание книги")
+    isbn = models.CharField('ISBN', max_length=13, help_text="13 символов")
+    genre = models.ManyToManyField(Genre, help_text="Выберите жанр книги")
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+
+        return self.title
+
+    def get_absolute_url(self):
+
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+
+        return ', '.join([genre.name for genre in self.genre.all()[:3] ])
+
+    display_genre.short_description = 'Genre'
+
+
+class BookInstance(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Уникальный ID указаной книги")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Обслуживание'),
+        ('o', 'Взаймы'),
+        ('a', 'Имеется в наличии'),
+        ('r', 'Зарезервированна'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text="Доступность книги")
+
+    class Meta:
+        ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+
+        return '{0} ({1})'.format(self.id, self.book.title)
+
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+
+        return '{0}, {1}'.format(self.last_name, self.first_name)
+
+
+class Language(models.Model):
+
+    name = models.CharField(max_length=200, help_text="Язык которым написана книга")
+
+    def __str__(self):
+        return self.name
+
+
+
